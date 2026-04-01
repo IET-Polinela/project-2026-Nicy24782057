7f98769ccccd8242102d35848890177981dda597
nicy24782057 <aurawarior@gmail.com>
2026-04-01 14:13:32 +0700

    menarapihkan file

--- npm24782057_iet_2026/npm24782057_iet_2026/settings.py
+++ npm24782057_iet_2026/npm24782057_iet_2026/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'npm24782057_iet_2026.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartcity_db',
+        'USER': 'postgres',
+        'PASSWORD': 'nicy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
